5852cacacb2be9f141418d7ba08d86cd1b7ba81a
Victor Zanda <victorzandal@MacBook-Pro-de-Victor-2.local>
2025-01-05 10:13:52 +0100

    Added gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,7 @@
-bind = "0.0.0.0:10000"
-workers = 4
-timeout = 120
+# gunicorn_config.py
+import os
+
+bind = f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"
+workers = 4  # número de workers
+timeout = 120  # timeout en segundos
+worker_class = 'sync'
