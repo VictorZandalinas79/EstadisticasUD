58bee917f7af7b40b309a74fc7341d5dcb656f69
Victor Zanda <victorzanpra@gmail.com>
2025-04-04 16:56:06 +0200

    cambios gunicorn1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,17 +5,23 @@ bind = "0.0.0.0:10000"
 
 # Número de workers (procesos) que Gunicorn lanzará
 # Para planes gratuitos de Render con recursos limitados, menos workers pueden ser más eficientes
-workers = 2
+workers = 1
 
 # Timeout en segundos
 timeout = 120
 
-# Clase de worker a utilizar
-# En caso de problemas con gevent, puedes usar un worker alternativo como 'sync'
+# Clase de worker a utilizar - usado el worker sync que es más básico pero confiable
 worker_class = "sync"
 
 # Nivel de log
 loglevel = "info"
 
-# Permite a Gunicorn recargar automáticamente cuando detecta cambios en el código
-reload = True
+# Threads por worker
+threads = 2
+
+# Tiempo de precarga
+preload_app = True
+
+# Opciones de worker
+max_requests = 1000
+max_requests_jitter = 50
